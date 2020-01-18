52ab5afc8ed7cf14ea6d04e5320d48e11e9c95eb
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-01-18 23:13:08 +0100

    js Stalker assignment

---
+++ JS stalker/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("what is your last name?");
+var age = prompt("what is your age?");
+
+console.log("My Name is " + firstName + " " + lastName);
+console.log("I am " + age + " years old");
