dbc3e1568a4062e3e09311b94f3d131fad7959f8
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-01-20 14:42:19 +0100

    prints even numbers between 10 and 40

--- js tasks/while loops/loops.js
+++ js tasks/while loops/loops.js
@@ -6,3 +6,12 @@ while (num < 20){
     num++;   
 }
 */
+
+// prints even numbers from 10 to 40
+var counter = 10;
+while (counter <= 40){
+    if(counter % 2 === 0){
+        console.log(counter);
+    }
+    counter += 1;
+} 
