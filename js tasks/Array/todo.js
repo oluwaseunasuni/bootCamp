b08fb476e855a037908de3468f81a37ad2ee232d
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-01-23 11:24:27 +0100

    added delete function and index

--- js tasks/Array/todo.js
+++ js tasks/Array/todo.js
@@ -5,14 +5,40 @@ var input = prompt("what would you like to do?");
 while(input !== "quit") {
     //handle input
     if(input === "list") {
-        console.log(todos);
-    } else if(input === "new") {
-        //ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //add todos to array
-        todos.push(newTodo);
-    }
-    //ak again for new input
+        listTodos();
+    }   else if(input === "new") {
+        addTodo();
+    }   else if(input === "delete") {
+        deleteTodo();
+        } 
+        //ak again for new input
     input = prompt("what would you like to do?");
 }
 console.log("OK, YOU QUIT THE APP");
+ 
+function listTodos(){
+    console.log("**********")
+    todos.forEach(function(todo, index){
+        console.log(index + ": " + todo);
+    });
+    console.log("**********")
+}
+
+function addTodo(){
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to Todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+
+}
+
+function deleteTodo() {
+    //ask for new todo
+    var index = prompt("Enter index of todo to delete");
+    // delete that todo
+    // splice
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+
+}
